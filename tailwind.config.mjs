a9c53fe0df66a3decec48285d632be41aa2932bd
David <davidpeinadorv@gmail.com>
2025-06-06 21:52:49 +0100

    feat: preload Frauces and remove Lora font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
-        serif: ["Fraunces Variable", "Lora", ...defaultTheme.fontFamily.serif],
+        serif: ["Fraunces Variable", ...defaultTheme.fontFamily.serif],
         display: ["Fraunces Variable", ...defaultTheme.fontFamily.serif],
       },
     },
