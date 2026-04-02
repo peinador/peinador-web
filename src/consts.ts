94c4ffc80bbe0ae0e0b3c204e0d0c8d49e92080d
David Peinador <davidpeinadorv@gmail.com>
2026-04-02 21:56:22 +0100

    add rss feed, change blog title and change title of type post

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@ import type { Site, Metadata, Socials } from "@types";
 export const SITE: Site = {
   NAME: "David Peinador",
   EMAIL: "web@peinador.uk",
-  YEAR: "2025",
+  YEAR: "2026",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
